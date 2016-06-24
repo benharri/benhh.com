0835d1a46634835dc1450cd4f1c581146ae732a8
benharri <benharri@nmu.edu>
2016-06-24 15:54:52 -0400

    update blog header

--- web/index.php
+++ web/index.php
@@ -17,10 +17,10 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => array(
-        'driver'   => 'pdo_sqlite',
-        'path'     => __DIR__.'/app.db',
-    ),
+  'db.options' => array(
+    'driver'   => 'pdo_sqlite',
+    'path'     => __DIR__.'/app.db',
+  ),
 ));
 
 
@@ -49,10 +49,6 @@ $app->get('/about/', function() use($app) {
   return $app['twig']->render('about.twig');
 });
 
-// contact
-$app->get('/contact', function() use($app) {
-  return $app['twig']->render('contact.twig');
-});
 
 // blog (grav)
 $app->get('/blog/[{slug}/]', function() {
@@ -74,16 +70,13 @@ $app->get('/solitaire/', function() use($app) {
 
 // pattern book
 $patternbook = $app['controllers_factory'];
-
 $patternbook->get('/', function() use($app) {
   return $app['twig']->render('patternbook/index.twig');
 });
-
-$patternbook->get('/{pattern}', function($pattern) use($app) {
+$patternbook->get('/{pattern}/', function($pattern) use($app) {
   require "pattern.php";
   return $app['twig']->render('patternbook/pattern.twig', ['pattern' => $pattern_info]);
 });
-
 $app->mount('/patternbook/', $patternbook);
 
 

7919cae8220e9f61fbd081d48c0755b79fdc60de
benharri <benharri@nmu.edu>
2016-07-20 01:49:18 -0400

    disable debug mode and publish day two

--- app/app.php
+++ app/app.php
@@ -2,7 +2,7 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
-$app['debug'] = true;
+// $app['debug'] = true;
 
 ///////////////////////////////////////////////////////////////////////
 // SERVICE PROVIDERS
